e61700ef8efa893925fc40d348f14cda5a2f5bff
Boamah-Powers <kb4242@nyu.edu>
2024-06-13 15:51:04 +0000

    app bar

--- expense_tracker/lib/screens/home/views/home_screen.dart
+++ expense_tracker/lib/screens/home/views/home_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:expense_tracker/screens/home/views/main_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -48,6 +49,7 @@ class HomeScreen extends StatelessWidget {
                 )),
             child: const Icon(CupertinoIcons.add)),
       ),
+      body: const MainScreen(),
     );
   }
 }
