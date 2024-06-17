5914ced9304a6d245195fc442c77f58d24c23c55
Boamah-Powers <kb4242@nyu.edu>
2024-06-17 23:19:52 +0000

    add expense page

---
+++ expense_tracker/lib/screens/add_expense/views/add_expense.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class AddExpense extends StatelessWidget {
+  const AddExpense({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.background,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Add Expenses',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500,
+                )
+              ),
+              const SizedBox(height: 16.0,),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.7,
+                child: TextFormField(
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    prefixIcon: const Icon(FontAwesomeIcons.dollarSign, size: 16.0,),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16.0,),
+              TextFormField(),
+              const SizedBox(height: 16.0,),
+              TextFormField(),
+              const SizedBox(height: 16.0,),
+              TextButton(
+                onPressed: () {
+      
+                }, 
+                child: const Text(
+                  'Save',
+                )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
